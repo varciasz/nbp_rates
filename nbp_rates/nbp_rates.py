9a841c366b3ba54b19d707f3609f5b47c473af1d
varciasz <varciasz@wp.pl>
2026-06-06 05:41:19 +0200

    RUB exception handling + small optimizations

--- nbp_rates/nbp_rates.py
+++ nbp_rates/nbp_rates.py
@@ -1,9 +1,9 @@
-#file generated at 2026-06-06 01:07:34
+#file generated at 2026-06-06 05:24:02
 from importlib import import_module
 from datetime import date, datetime, timedelta
 
-TABLE_A_CURRENCIES = frozenset({'MXN', 'LVL', 'CLP', 'HUF', 'AUD', 'THB', 'EUR', 'BRL', 'UAH', 'RON', 'ILS', 'NOK', 'SGD', 'HRK', 'ZAR', 'JPY', 'BGN', 'NZD', 'USD', 'TRY', 'CNY', 'ISK', 'XDR', 'CHF', 'KRW', 'IDR', 'CAD', 'PHP', 'HKD', 'CZK', 'DKK', 'RUB', 'MYR', 'SEK', 'LTL', 'INR', 'GBP'})
-TABLE_B_CURRENCIES = frozenset({'SLE', 'FJD', 'KWD', 'XOF', 'KHR', 'NIO', 'PGK', 'XAF', 'CVE', 'ANG', 'VEF', 'NPR', 'VUV', 'LAK', 'BBD', 'ZWL', 'SAR', 'ETB', 'YER', 'BWP', 'GEL', 'TND', 'HTG', 'IRR', 'PAB', 'CDF', 'PEN', 'MMK', 'BHD', 'MRO', 'GYD', 'XPF', 'MUR', 'RSD', 'CRC', 'UYU', 'MVR', 'SDG', 'STD', 'MKD', 'LYD', 'MZN', 'TMT', 'BAM', 'TOP', 'SYP', 'PYG', 'SOS', 'BYN', 'MOP', 'BND', 'OMR', 'SSP', 'SCR', 'SLL', 'AFN', 'GNF', 'GIP', 'DOP', 'RWF', 'XCG', 'BZD', 'BOB', 'AWG', 'TWD', 'ALL', 'GTQ', 'QAR', 'COP', 'TTD', 'DJF', 'EGP', 'ZWG', 'GHS', 'PKR', 'GMD', 'DZD', 'NAD', 'KMF', 'SZL', 'LSL', 'RUB', 'MNT', 'MRU', 'WST', 'UGX', 'CUP', 'STN', 'UZS', 'ERN', 'SVC', 'KGS', 'LKR', 'BDT', 'VES', 'SRD', 'VND', 'MWK', 'JMD', 'MGA', 'KES', 'TZS', 'KZT', 'ZMK', 'AED', 'ARS', 'AZN', 'MAD', 'XCD', 'AMD', 'BIF', 'BSD', 'SBD', 'LBP', 'AOA', 'TJS', 'BYR', 'NGN', 'JOD', 'ZMW', 'HNL', 'MDL', 'LRD', 'IQD'})
+TABLE_A_CURRENCIES = frozenset({'HUF', 'DKK', 'NOK', 'THB', 'NZD', 'KRW', 'BRL', 'LVL', 'SEK', 'MYR', 'USD', 'RUB', 'JPY', 'UAH', 'MXN', 'RON', 'CAD', 'CLP', 'ZAR', 'HRK', 'BGN', 'AUD', 'IDR', 'HKD', 'LTL', 'INR', 'PHP', 'SGD', 'TRY', 'ISK', 'EUR', 'CZK', 'XDR', 'GBP', 'CNY', 'ILS', 'CHF'})
+TABLE_B_CURRENCIES = frozenset({'KMF', 'BND', 'VEF', 'MKD', 'LYD', 'SCR', 'VUV', 'AOA', 'ERN', 'RSD', 'KHR', 'XCG', 'BIF', 'NIO', 'PYG', 'BDT', 'TJS', 'PAB', 'TWD', 'GNF', 'CRC', 'SOS', 'TZS', 'RWF', 'BYR', 'ANG', 'WST', 'LAK', 'XCD', 'AMD', 'BAM', 'NPR', 'ZMW', 'BSD', 'AWG', 'CUP', 'SZL', 'ARS', 'HNL', 'MGA', 'ZWG', 'GTQ', 'JOD', 'ALL', 'KGS', 'CVE', 'PKR', 'AZN', 'VND', 'DOP', 'IRR', 'HTG', 'LKR', 'SLL', 'SLE', 'SAR', 'XOF', 'SDG', 'KWD', 'BOB', 'MNT', 'XPF', 'ZWL', 'MRO', 'JMD', 'MWK', 'UZS', 'SRD', 'SYP', 'TND', 'MDL', 'PEN', 'GMD', 'STD', 'LRD', 'KES', 'MRU', 'BWP', 'CDF', 'KZT', 'FJD', 'LSL', 'BHD', 'XAF', 'PGK', 'SSP', 'GHS', 'ETB', 'TTD', 'MAD', 'MMK', 'NGN', 'GIP', 'BZD', 'VES', 'STN', 'IQD', 'UYU', 'NAD', 'TMT', 'GEL', 'SVC', 'MUR', 'AED', 'EGP', 'RUB', 'MOP', 'COP', 'TOP', 'QAR', 'YER', 'DJF', 'ZMK', 'MVR', 'GYD', 'LBP', 'MZN', 'OMR', 'BBD', 'AFN', 'SBD', 'BYN', 'DZD', 'UGX'})
 
 def ProvideCurrencyRate(date_given, currency, fallback=False, _depth=0):
     """
@@ -25,14 +25,16 @@ def ProvideCurrencyRate(date_given, currency, fallback=False, _depth=0):
     else:
         raise ValueError('date_given must be a datetime.date object or a string in YYYY-MM-DD format')
 
-    if currency == 'USD' or currency == 'EUR': table_name = 'A'
-    elif currency in TABLE_A_CURRENCIES: table_name = 'A'
-    elif currency in TABLE_B_CURRENCIES: table_name = 'B'
-    else: raise ValueError('Error - wrong currency')
-
     if date_given > date(2026,6,5):
+        if currency == 'USD' or currency == 'EUR': table_name = 'A'
+        elif currency in TABLE_A_CURRENCIES: table_name = 'A'
+        elif currency in TABLE_B_CURRENCIES: table_name = 'B'
+        else: raise ValueError('Error - wrong currency')
+
         from .online_fetcher import fetch_rate_from_nbp
         rate = fetch_rate_from_nbp(date_given, currency, table_name)
+        if currency == 'RUB' and rate == '-1':  # RUB is in both tables and we want to fallback to the opposite table if rate is not found for the provided date (e.g. due to holidays or mid-year switch) - this logic is implemented in fetch_rate_from_nbp function
+            rate = fetch_rate_from_nbp(date_given, currency, {'A': 'B', 'B': 'A'}[table_name]) #run also for the opposite table if rate is not found
     else:
         try:
             module = import_module(f'nbp_rates.currencies.curr_{currency.lower()}')
